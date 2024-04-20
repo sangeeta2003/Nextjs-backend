7479401376b9bd890ff7256be30d2b9612282e99
*pknowledge* <mishrasangeeta944@gmail.com>
2024-04-20 21:56:13 +0530

    loading page

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const loading = () => {
+  return ( <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            Loading...
+        </div>
+    </div>
+  )
+}
+
+export default loading
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
 import axios from "axios";
+import { setTimeout } from "timers/promises";
 async function getUserDetails() {
+  await new Promise((r) => setTimeout( r,5000));
   const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
 	return response.data;
 }
 export default async function Home() {
-  const userDeatils = await getUserDetails();
+  const userData = await getUserDetails();
   return (
-    <div>
-      hey there
-      {userDeatils.email}
-      {userDeatils.name}
-
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
     </div>
   );
 }
