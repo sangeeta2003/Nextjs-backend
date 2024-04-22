d70df2a50f2c360dab8170d53c2296b65621c1b6
*pknowledge* <mishrasangeeta944@gmail.com>
2024-04-22 09:06:51 +0530

    useRouter

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,10 +1,7 @@
-import { NextRequest } from "next/server";
-import { Response } from "express";
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
-    console.log(body);
-    return new Response().json({
-        message: "You are logged in!"
-    });
+
+    return NextResponse.json({ username: body.username, password: body.password })
 }
